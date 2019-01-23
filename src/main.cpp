aa7b501d14c8d35cc044bd909615ce4d675f5ec0
Linjian Li <43627450+LinjianLi@users.noreply.github.com>
2019-01-23 19:20:16 +0800

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -11,37 +11,61 @@ int main() {
 
 	st.test1();
 
-	// Redirect std::cout.
+	cout << "Do you want to redirect std::cout to a text file? \n"
+	     << "Please input [y/n]: " ;
+	char redirect;
+	cin >> redirect;
+
 	string log_file = "log_bayesian_network_program.txt";
-	std::cout << '\n' << "The std::cout will be redirect to " << log_file << endl;
+	std::streambuf *coutbuf = std::cout.rdbuf();  //save old buf
 	std::ofstream out(log_file);
-	std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
-	std::cout.rdbuf(out.rdbuf()); //redirect std::cout to log_bayesian_network.txt!
+
+	if (redirect=='y' || redirect=='Y') {
+		// Redirect std::cout.
+		std::cout << '\n' << "The std::cout will be redirect to " << log_file << endl;
+		std::cout.rdbuf(out.rdbuf()); //redirect std::cout to log_bayesian_network.txt!
+	}
 
 	time_t now = time(0);
 	char* dt = ctime(&now);
 	cout << "Log file last modified at " << dt << endl;
 
-	Trainer *trainer = new Trainer();
-	trainer->loadLIBSVMData_AutoDetectConfig("../data/a1a.txt");    // Clion looks for file from Debug folder.
+	auto *trainer = new Trainer();
+	auto *tester = new Trainer();
+	auto *network = new Network();
 
-	Trainer *tester = new Trainer();
-	tester->loadLIBSVMData_AutoDetectConfig("../data/a1a.test.txt");
+	string train_set_file_path, test_set_file_path;
 
-	Network* network = new Network();
-	network->structLearn_ChowLiu_CompData(trainer);
-	network->trainNetwork_KnowStruct_CompData(trainer);
-	network->testingNetworkReturnAccuracy(tester);
+	train_set_file_path =
+				//	"../data/a1a.txt"
+				//	"../data/a2a.txt"
+					"../data/a3a.txt"
+				//	"../data/w1a.txt"
+				//	"../data/w7a.txt"
+					;
 
-	st.test2(network);
+	test_set_file_path =
+				//	"../data/a1a.test.txt"
+				//	"../data/a2a.test.txt"
+					"../data/a3a.test.txt"
+				//	"../data/w1a.test.txt"
+				//	"../data/w7a.test.txt"
+					;
+
+	trainer->LoadLIBSVMDataAutoDetectConfig(train_set_file_path);
+	tester->LoadLIBSVMDataAutoDetectConfig(test_set_file_path);
+	network->StructLearnChowLiuTreeCompData(trainer);
+	network->LearnParmsKnowStructCompData(trainer);
+	network->TestNetReturnAccuracy(tester);
 
 	now = time(0);
 	dt = ctime(&now);
 	cout << '\n' << "Log file last modified at " << dt << endl;
 
-	std::cout.rdbuf(coutbuf); //reset to standard output again
-	std::cout << "The std::cout has been reset to standard output again." << endl;
-
+	if (redirect=='y' || redirect=='Y') {
+		std::cout.rdbuf(coutbuf); //reset to standard output again
+		std::cout << "The std::cout has been reset to standard output again." << endl;
+	}
 	std::cout << "Hello, World!" << std::endl;
 
 	return 0;
