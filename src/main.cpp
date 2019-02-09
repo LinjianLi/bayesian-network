4c528e6966181e83a9e001388d0624ecedba010d
Linjian Li <linjiann.li@gmail.com>
2019-02-09 10:56:27 +0800

    Begin working on continuous nodes.
    Also, refactor indentation characters (from tab to space, according to Google C++ Style Guide) to improve maintainability.

--- src/main.cpp
+++ src/main.cpp
@@ -8,70 +8,70 @@
 
 int main() {
 
-	SomeTest st;
+  SomeTest st;
 
-	st.test1();
+  st.test1();
 
-	cout << "Do you want to redirect std::cout to a text file? \n"
-	     << "Please input [y/n]: " ;
-	char redirect;
-	cin >> redirect;
+  cout << "Do you want to redirect std::cout to a text file? \n"
+       << "Please input [y/n]: " ;
+  char redirect;
+  cin >> redirect;
 
-	string log_file = "log_bayesian_network_program.txt";
-	std::streambuf *coutbuf = std::cout.rdbuf();  //save old buf
-	std::ofstream out(log_file);
+  string log_file = "log_bayesian_network_program.txt";
+  std::streambuf *coutbuf = std::cout.rdbuf();  //save old buf
+  std::ofstream out(log_file);
 
-	if (redirect=='y' || redirect=='Y') {
-		// Redirect std::cout.
-		std::cout << '\n' << "The std::cout will be redirect to " << log_file << endl;
-		std::cout.rdbuf(out.rdbuf()); //redirect std::cout to log_bayesian_network.txt!
-	}
+  if (redirect=='y' || redirect=='Y') {
+    // Redirect std::cout.
+    std::cout << '\n' << "The std::cout will be redirect to " << log_file << endl;
+    std::cout.rdbuf(out.rdbuf()); //redirect std::cout to log_bayesian_network.txt!
+  }
 
-	time_t now = time(0);
-	char* dt = ctime(&now);
-	cout << "Log file last modified at " << dt << endl;
+  time_t now = time(0);
+  char* dt = ctime(&now);
+  cout << "Log file last modified at " << dt << endl;
 
-	auto *trainer = new Trainer();
-	auto *tester = new Trainer();
-	auto *network = new ChowLiuTree();
+  auto *trainer = new Trainer();
+  auto *tester = new Trainer();
+  auto *network = new ChowLiuTree();
 
-	string train_set_file_path, test_set_file_path;
+  string train_set_file_path, test_set_file_path;
 
-	// Note that Clion search file starting at "cmake-build-debug" folder.
+  // Note that Clion search file starting at "cmake-build-debug" folder.
 
-	train_set_file_path =
-				//	"../data/a1a.txt"
-				//	"../data/a2a.txt"
-					"../data/a3a.txt"
-				//	"../data/w1a.txt"
-				//	"../data/w7a.txt"
-					;
+  train_set_file_path =
+        //  "../data/a1a.txt"
+        //  "../data/a2a.txt"
+          "../data/a3a.txt"
+        //  "../data/w1a.txt"
+        //  "../data/w7a.txt"
+          ;
 
-	test_set_file_path =
-				//	"../data/a1a.test.txt"
-				//	"../data/a2a.test.txt"
-					"../data/a3a.test.txt"
-				//	"../data/w1a.test.txt"
-				//	"../data/w7a.test.txt"
-					;
+  test_set_file_path =
+        //  "../data/a1a.test.txt"
+        //  "../data/a2a.test.txt"
+          "../data/a3a.test.txt"
+        //  "../data/w1a.test.txt"
+        //  "../data/w7a.test.txt"
+          ;
 
-	trainer->LoadLIBSVMDataAutoDetectConfig(train_set_file_path);
-	tester->LoadLIBSVMDataAutoDetectConfig(test_set_file_path);
-	network->StructLearnCompData(trainer);
-	network->LearnParmsKnowStructCompData(trainer);
-	network->TestNetReturnAccuracy(tester);
+  trainer->LoadLIBSVMDataAutoDetectConfig(train_set_file_path);
+  tester->LoadLIBSVMDataAutoDetectConfig(test_set_file_path);
+  network->StructLearnCompData(trainer);
+  network->LearnParmsKnowStructCompData(trainer);
+  network->TestNetReturnAccuracy(tester);
 
-	now = time(0);
-	dt = ctime(&now);
-	cout << '\n' << "Log file last modified at " << dt << endl;
+  now = time(0);
+  dt = ctime(&now);
+  cout << '\n' << "Log file last modified at " << dt << endl;
 
-	if (redirect=='y' || redirect=='Y') {
-		std::cout.rdbuf(coutbuf); //reset to standard output again
-		std::cout << "The std::cout has been reset to standard output again." << endl;
-	}
-	std::cout << "Hello, World!" << std::endl;
+  if (redirect=='y' || redirect=='Y') {
+    std::cout.rdbuf(coutbuf); //reset to standard output again
+    std::cout << "The std::cout has been reset to standard output again." << endl;
+  }
+  std::cout << "Hello, World!" << std::endl;
 
-	return 0;
+  return 0;
 
 
 }
