85b9f1c470f2b16c029311e8a62f7c8b8a3016ef
LinjianLi <linjiann.li@gmail.com>
2019-03-14 19:10:01 +0800

    Use Google Test.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
-#include "SomeTest.h"
 #include "Trainer.h"
 #include "Network.h"
 #include "ChowLiuTree.h"
@@ -26,45 +25,6 @@ int main() {
   time_t now = time(0);
   char* dt = ctime(&now);
   cout << "Log file last modified at " << dt << endl;
-
-  SomeTest st;
-
-  //st.test1();
-
-
-  auto *trainer = new Trainer();
-  auto *tester = new Trainer();
-  auto *network = new ChowLiuTree();
-
-  string train_set_file_path, test_set_file_path;
-
-  // Note that Clion search file starting at "cmake-build-debug" folder.
-
-  train_set_file_path =
-        //  "../data/a1a.txt"
-        //  "../data/a2a.txt"
-          "../data/a3a.txt"
-        //  "../data/w1a.txt"
-        //  "../data/w7a.txt"
-          ;
-
-  test_set_file_path =
-        //  "../data/a1a.test.txt"
-        //  "../data/a2a.test.txt"
-          "../data/a3a.test.txt"
-        //  "../data/w1a.test.txt"
-        //  "../data/w7a.test.txt"
-          ;
-
-  trainer->LoadLIBSVMDataAutoDetectConfig(train_set_file_path);
-  tester->LoadLIBSVMDataAutoDetectConfig(test_set_file_path);
-  network->StructLearnCompData(trainer);
-  network->LearnParmsKnowStructCompData(trainer);
-  //network->TestNetReturnAccuracy(tester);
-
-
-  //st.test2(network, tester, 0);
-  st.test3(network, 0);
 
 
   now = time(0);
